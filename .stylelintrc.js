82eca06bf253d2680eae53313eae9752ee9fc4f3
Masayuki <soy2.san@gmail.com>
2021-09-12 17:04:30 +0900

    🎉  add eslint prettier stylelint sass

---
+++ .stylelintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: ['stylelint-scss'],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-prettier/recommended',
+  ],
+  rules: {
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
+  }
+}
