c7b007de67cfac77a227cf2c8359a31726a9228d
Masayuki <soy2.san@gmail.com>
2021-09-13 21:43:51 +0900

    ✨  add template

---
+++ src/components/templates/UserData.tsx
@@ -0,0 +1,13 @@
+import React, { VFC } from 'react'
+
+const UserData: VFC = () => {
+  return (
+    <>
+      <span style={{ color: '#51C4D3' }}>User</span>
+      <span style={{ color: '#126E82' }}>@</span>
+      <span style={{ color: '#132C33' }}>btools</span>
+    </>
+  );
+}
+
+export default UserData
